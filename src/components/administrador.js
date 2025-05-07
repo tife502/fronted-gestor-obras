5c7d5b488d9648652267215d8176f17426eb9b36
Solau03 <150872507+Solau03@users.noreply.github.com>
2025-05-06 21:31:07 -0500

    Update administrador.js

--- src/components/administrador.js
+++ src/components/administrador.js
@@ -35,7 +35,7 @@ function Administrador() {
       console.log('userId:', userId);
 
       
-      const responseUsuarios = await axios.get("http://localhost:5000/api/usuarios/mostrarusuarios");
+      const responseUsuarios = await axios.get("https://gestordeobras-3.onrender.com/api/usuarios/mostrarusuarios");
       console.log("Usuarios obtenidos:", responseUsuarios.data); 
 
       // Si es un administrador (rolId == 1), carga todos los usuarios
@@ -48,7 +48,7 @@ function Administrador() {
       setUsuarios(usuariosFiltrados);
 
      
-      const responseZonas = await axios.get("http://localhost:5000/api/zonas/mostrarzonas");
+      const responseZonas = await axios.get("https://gestordeobras-3.onrender.com/api/zonas/mostrarzonas");
       console.log("Zonas obtenidas:", responseZonas.data); 
       setZonas(responseZonas.data);
       
@@ -67,7 +67,7 @@ function Administrador() {
   // Función para eliminar usuario
   const eliminarUsuario = async (id) => {
     try {
-      await axios.delete(`http://localhost:5000/api/usuarios/eliminarusuario/${id}`);
+      await axios.delete(`https://gestordeobras-3.onrender.com/api/usuarios/eliminarusuario/${id}`);
       setUsuarios((prevUsuarios) => prevUsuarios.filter((usuario) => usuario.id !== id));
     } catch (error) {
       console.error("Error al eliminar usuario:", error.response?.data || error.message);
@@ -84,7 +84,7 @@ function Administrador() {
     if (!editUser) return;
 
     try {
-      const response = await fetch(`http://127.0.0.1:5000/api/usuarios/modificarusuario/${id}`, {
+      const response = await fetch(`https://gestordeobras-3.onrender.com/api/usuarios/modificarusuario/${id}`, {
         method: "PATCH",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
